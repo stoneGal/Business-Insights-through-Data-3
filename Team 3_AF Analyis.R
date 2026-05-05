80576c4e9d412f60e77577c2d9188bdec0a6453f
oceanareyes <oreyes1@student.hult.edu>
2026-05-05 09:22:27 -0700

    Starting Project Files
    Adding starting files for Team 3 - R project file, R script, and the spreadsheet supplement from the Air France Business Case.

---
+++ Team 3_AF Analyis.R
@@ -0,0 +1,20 @@
+
+###################################################
+###################################################
+### Air France Business Case Analysis
+### Created by Team 3:
+### Bethel Abah
+### Andrés Cravioto
+### Alejandro Espinoza
+### Oceana Reyes
+### created on: 05/5/2026
+###################################################
+###################################################
+
+#Importing data from the Air France case spreadsheet
+
+library(readxl)
+Air_France_Case_Spreadsheet_Supplement <- read_excel("Air France Case Spreadsheet Supplement.xls", 
+                                                     sheet = "DoubleClick")
+View(Air_France_Case_Spreadsheet_Supplement)
+
