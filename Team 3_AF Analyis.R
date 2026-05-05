58bb0670758e9caed5d365b921e31e2093f006b0
ancrato <ancrato24@gmail.com>
2026-05-05 14:30:27 -0600

    Update Team 3_AF Analyis.R

--- Team 3_AF Analyis.R
+++ Team 3_AF Analyis.R
@@ -27,5 +27,48 @@ airfrancedf$NET_Amount <- airfrancedf$Amount - airfrancedf$`Total Cost`
 #Creating an NET ROA Vector
 airfrancedf$NET_ROA <- airfrancedf$NET_Amount / airfrancedf$`Total Cost`
 
+#Applying business logic to ROA
+#Benchmark is 4
+#Defining success into a binary vector
+airfrancedf$NET_ROA_binary <- ifelse(airfrancedf$NET_ROA >= 4, 
+                                     1, 0)
+
+#Applying business logic to CPC
+#Benchmark is lower than 2
+#Defining success into a binary vector
+airfrancedf$CPC_binary <- ifelse(airfrancedf$`Avg. Cost per Click` <= 2, 
+                                 1, 0)
+
+#Applying business logic to Volume
+#Benchmark is greater than 5
+#Defining success into a binary vector
+airfrancedf$Bookings_binary <- ifelse(airfrancedf$`Total Volume of Bookings` >= 5, 
+                                      1, 0)
+
+#Applying business logic to CTR
+#Benchmark is greater than 8.24
+#Defining success into a binary vector
+airfrancedf$CTR_binary <- ifelse(airfrancedf$`Engine Click Thru %` >= 8.24, 
+                                 1, 0)
+
+#Generating a binary field that combines success in Volume, CPC and ROA.
+airfrancedf$Optimal_binary <- ifelse(airfrancedf$Bookings_binary == 1 & 
+                                       airfrancedf$CPC_binary == 1 & 
+                                       airfrancedf$NET_ROA_binary == 1, 
+                                     1, 0)
+
+
+#Figuring out Keywords
+print (airfrancedf$`Keyword Group`)
+
+#We need a field that checks if "Air France" is contained in the Keyword Group field. 
+#Creating a Branded vs Unbranded Field
+airfrancedf$Branded <- ifelse(grepl("Air France", airfrancedf$`Keyword Group`, 
+                                    ignore.case = TRUE), 1, 0)
+
+#Checking if it worked
+print (airfrancedf$Branded)
+View(airfrancedf[airfrancedf$Branded == 1, c("Keyword Group", "Branded")])
+View(airfrancedf[airfrancedf$Branded == 0, c("Keyword Group", "Branded")])
 
 
