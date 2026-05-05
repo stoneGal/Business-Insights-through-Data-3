0ecfe821fd23195af1698ec7888396cccd9a07d2
ancrato <ancrato24@gmail.com>
2026-05-05 13:22:06 -0600

    Update Team 3_AF Analyis.R
    Changed the name of the DF to be shorter.

--- Team 3_AF Analyis.R
+++ Team 3_AF Analyis.R
@@ -12,9 +12,20 @@
 ###################################################
 
 #Importing data from the Air France case spreadsheet
-
 library(readxl)
-Air_France_Case_Spreadsheet_Supplement <- read_excel("Air France Case Spreadsheet Supplement.xls", 
+air_france_df <- read_excel("Air France Case Spreadsheet Supplement.xls", 
                                                      sheet = "DoubleClick")
-View(Air_France_Case_Spreadsheet_Supplement)
+View(air_france_df)
+
+#Creating basic scalars
+num_ads <- nrow(airfrancedf)
+num_obs <- ncol(airfrancedf)
+
+#Creating an NET Amount vector
+airfrancedf$NET_Amount <- airfrancedf$Amount - airfrancedf$`Total Cost`
+
+#Creating an NET ROA Vector
+airfrancedf$NET_ROA <- airfrancedf$NET_Amount / airfrancedf$`Total Cost`
+
+
 
